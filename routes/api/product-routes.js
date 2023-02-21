e0d9ffa4fd1641d90dd0765209834b40575b76ae
Jacob Roth <jakeroth0@gmail.com>
2023-02-21 11:41:14 -0500

    all routes have been updated and are working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,19 +22,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id` 
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: {
-        model: Category,
-        where: {id: req.params.id},
-        attributes: ['id', 'category_name']
-      },
-      include: {
-        model: Tag,
-        through: {
-          model: ProductTag,
-          where: {product_id: req.params.id},
-          attributes: []
-        } 
-      }
+      include: [Category, Tag]
     });
     if (!productData) {
       res.status(404).json({ message: 'No product with this id'});
@@ -121,8 +109,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
