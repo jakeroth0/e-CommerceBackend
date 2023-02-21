3b1e796b9868e6ac5898b0420df821d0e77523c5
Jacob Roth <jakeroth0@gmail.com>
2023-02-20 22:27:41 -0500

    added basic get api requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,15 +1,32 @@
 const router = require('express').Router();
+const sequelize = require('sequelize');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id);
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
